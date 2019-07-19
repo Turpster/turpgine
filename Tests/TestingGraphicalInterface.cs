6ed571800b9536259539f8e42d8798d6497c0b14
Turpster <turpster@turpster.xyz>
2019-07-19 09:19:10 +0100

    Added tests

---
+++ Tests/TestingGraphicalInterface.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using Engine.Graphics.Interface;
+using Engine.Graphics.Model;
+using Engine.Graphics.Model._3D;
+using OpenTK;
+
+namespace Tests
+{
+    public class TestingGraphicalInterface : GraphicalInterface
+    {
+        public TestingGraphicalInterface(GraphicalManager graphicalManager) : base(graphicalManager, "Testing Interface")
+        {
+            InitObjects();
+        }
+
+        private void InitObjects()
+        {
+            var vertices = new List<Vertex>
+            {
+                new Vertex(new Vector3(0.5f, -0.5f, 1.0f)),
+                new Vertex(new Vector3(0.0f, 0.5f, 1.0f)),
+                new Vertex(new Vector3(-0.5f, -0.5f, 1.0f))
+            };
+            
+            ModelManager.Add(new Model3D(ModelManager, new Mesh3D(vertices)));
+        }
+    }
+}
