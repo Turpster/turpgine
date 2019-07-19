aee246d5c862a7bc043b5cd40de6d87d7162df6d
Turpster <turpster@turpster.xyz>
2019-07-19 09:22:18 +0100

    Added interfce to GraphicalInterfaceManager

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -1,24 +1,12 @@
-﻿using System.Collections.Generic;
-using Engine.Graphics.Model;
-using Engine.Graphics.Model._3D;
-using OpenTK;
-
-namespace Tests
+﻿namespace Tests
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
-            var vertices = new List<Vertex>
-            {
-                new Vertex(new Vector3(0.5f, -0.5f, 1.0f)),
-                new Vertex(new Vector3(0.0f, 0.5f, 1.0f)),
-                new Vertex(new Vector3(-0.5f, -0.5f, 1.0f))
-            };
-
-            var mesh3D = new Mesh3D(vertices);
-
             var engine = new Engine.Engine();
+            
+            var testGraphicalInterface = new TestingGraphicalInterface(engine.GraphicalManager);
         }
     }
 }
