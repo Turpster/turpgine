2877c79e3078cdc738d6b192ebad255954bb99c2
Turpster <turpster@turpster.xyz>
2019-07-22 11:56:07 +0100

    Added GlInitialise to Shaders

---
+++ Engine/Graphics/GlObject.cs
@@ -0,0 +1,8 @@
+namespace Engine.Graphics
+{
+    public abstract class IGlObject
+    {
+        protected internal abstract void GlInitialise();
+        protected internal abstract void GlTerminate();
+    }
+}
